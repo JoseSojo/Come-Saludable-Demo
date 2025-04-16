515b5a2f564336992dbaaa2c3550c74537e62089
Jose Sojo <jsojo346@gmail.com>
2025-04-16 16:06:56 -0400

    fix(styles): agregar estilos en el input login

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -48,7 +48,7 @@ const Login: FC = () => {
                                     required
                                     pattern="[0-9]{7}"
                                     maxLength={7}
-                                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+                                    className="appearance-none block w-full px-3 py-2 border border-gray-600 text-gray-600 rounded-md shadow-sm placeholder-gray-600 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
                                     placeholder="1234567"
                                     value={formData.code}
                                     onChange={(e) => setFormData({ code: e.target.value })}
